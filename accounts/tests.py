053ee5a59949ed4d34a015f0813c62602f298724
wencharles <ndanyicharles14@gmail.com>
2019-09-04 18:35:58 +0300

    done singup tests- signup plus successful signup tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,50 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.test import TestCase
+from django.urls import reverse, resolve
+
 from .views import signup
 
-def SignUpTest(TestCase):
-    def sign_up_status_code()
+class SignUpTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_sign_up_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_sign_up_resolves_sign_up_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'john',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        '''A valid redirection should direct the user to the homepage'''
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''create a test to an abitrary page and the resulting page must have a user to its context after a successful login'''
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
